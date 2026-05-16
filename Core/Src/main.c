8733fa9c746eafc4353eedeb84d8090a4e0904ec
Pedroman17 <160240598+Pedroman17@users.noreply.github.com>
2026-05-16 18:10:57 +0300

    Add files via upload

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -40,7 +40,7 @@
 /* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
-ADC_HandleTypeDef hadc1;
+UART_HandleTypeDef huart3;
 
 /* USER CODE BEGIN PV */
 
@@ -49,7 +49,7 @@ ADC_HandleTypeDef hadc1;
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
-static void MX_ADC1_Init(void);
+static void MX_USART3_UART_Init(void);
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
@@ -88,9 +88,9 @@ int main(void)
 
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
-  MX_ADC1_Init();
+  MX_USART3_UART_Init();
   /* USER CODE BEGIN 2 */
-  volatile HAL_StatusTypeDef adcPoolResult; // status ADC
+
   /* USER CODE END 2 */
 
   /* Infinite loop */
@@ -98,22 +98,76 @@ int main(void)
   while (1)
   {
     /* USER CODE END WHILE */
-	uint32_t sum = 0; // variable for storing result
-	uint16_t avg = 0; // variable for the mean
-	float voltage = 0; // variable for the calculated value of the voltage
-	 for (int i = 0; i < 256; i++) // cycle of 265 iterations
-	 {
-		 HAL_ADC_Start(&hadc1); // start ADC
-		 adcPoolResult = HAL_ADC_PollForConversion(&hadc1, 10); // obtaining status
+	  uint8_t rcvBuf[1]; // buffer for 1 byte
+	  HAL_StatusTypeDef result;
 
-		 if (adcPoolResult == HAL_OK)
-			 sum += HAL_ADC_GetValue(&hadc1); // if the status ok adds a value to the variable
-	 }
+	  result = HAL_UART_Receive(&huart3, rcvBuf, 1, 10); // read 1 byte, time out 10 ms
 
-	 avg = sum >> 8; // divide the sum of the measurement values by 256
+	  if (result == HAL_OK) // if data received
+	  {
+		  switch (rcvBuf[0]) // process the command
+		  {
+		  	  case '1': // turn on green led
+		  		  HAL_GPIO_WritePin(GPIOD, GPIO_PIN_12, GPIO_PIN_SET); // green led on
+		  		  HAL_UART_Transmit(&huart3, (uint8_t*)"GREEN ON\r\n", 10, 100); // send message
+		  		  break; // exit switch-case block
 
-	 voltage = (avg / 4095.0f) * 3.3f; // calculate the voltage value
+		  	  case '2': // turn off green led
+		  		  HAL_GPIO_WritePin(GPIOD, GPIO_PIN_12, GPIO_PIN_RESET);
+		  		  HAL_UART_Transmit(&huart3, (uint8_t*)"GREEN OFF\r\n", 11, 100);
+		  		  break;
 
+		  	  case '3': // turn on orange led
+		  		  HAL_GPIO_WritePin(GPIOD, GPIO_PIN_13, GPIO_PIN_SET);
+		  	      HAL_UART_Transmit(&huart3, (uint8_t*)"ORANGE ON\r\n", 11, 100);
+		  		  break;
+
+		      case '4': // turn off orange led
+		          HAL_GPIO_WritePin(GPIOD, GPIO_PIN_13, GPIO_PIN_RESET);
+		  		  HAL_UART_Transmit(&huart3, (uint8_t*)"ORANGE OFF\r\n", 13, 100);
+		  		  break;
+
+		  	  case '5': // turn on red led
+		  		  HAL_GPIO_WritePin(GPIOD, GPIO_PIN_14, GPIO_PIN_SET);
+		  		  HAL_UART_Transmit(&huart3, (uint8_t*)"RED ON\r\n", 8, 100);
+		  		  break;
+
+		  	  case '6': // turn off red led
+		  		  HAL_GPIO_WritePin(GPIOD, GPIO_PIN_14, GPIO_PIN_RESET);
+		  		  HAL_UART_Transmit(&huart3, (uint8_t*)"RED OFF\r\n", 9, 100);
+		  		  break;
+
+		  	  case '7': // turn on blue led
+		  		  HAL_GPIO_WritePin(GPIOD, GPIO_PIN_15, GPIO_PIN_SET);
+		  		  HAL_UART_Transmit(&huart3, (uint8_t*)"BLUE ON\r\n", 9, 100);
+		  		  break;
+
+		  	  case '8': // turn off blue led
+		  		  HAL_GPIO_WritePin(GPIOD, GPIO_PIN_15, GPIO_PIN_RESET);
+		  		  HAL_UART_Transmit(&huart3, (uint8_t*)"BLUE OFF\r\n", 10, 100);
+		  		  break;
+
+		  	  case '9': // turn on all leds
+		  		 HAL_GPIO_WritePin(GPIOD, GPIO_PIN_12, GPIO_PIN_SET);
+		  		 HAL_GPIO_WritePin(GPIOD, GPIO_PIN_13, GPIO_PIN_SET);
+		  		 HAL_GPIO_WritePin(GPIOD, GPIO_PIN_14, GPIO_PIN_SET);
+		  		 HAL_GPIO_WritePin(GPIOD, GPIO_PIN_15, GPIO_PIN_SET);
+		  		 HAL_UART_Transmit(&huart3, (uint8_t*)"ALL ON\r\n", 8, 100);
+		  		 break;
+
+		  	  case '0': // turn off all leds
+		  		 HAL_GPIO_WritePin(GPIOD, GPIO_PIN_12, GPIO_PIN_RESET);
+		  		 HAL_GPIO_WritePin(GPIOD, GPIO_PIN_13, GPIO_PIN_RESET);
+		  		 HAL_GPIO_WritePin(GPIOD, GPIO_PIN_14, GPIO_PIN_RESET);
+		  		 HAL_GPIO_WritePin(GPIOD, GPIO_PIN_15, GPIO_PIN_RESET);
+		  		 HAL_UART_Transmit(&huart3, (uint8_t*)"ALL OFF\r\n", 9, 100);
+		  		 break;
+
+		  	  default: // unknown command
+		  		 HAL_UART_Transmit(&huart3, (uint8_t*)"UNKNOWN COMMAND!\r\n", 18, 100);
+		  		 break;
+		  }
+	  }
     /* USER CODE BEGIN 3 */
   }
   /* USER CODE END 3 */
@@ -161,54 +215,35 @@ void SystemClock_Config(void)
 }
 
 /**
-  * @brief ADC1 Initialization Function
+  * @brief USART3 Initialization Function
   * @param None
   * @retval None
   */
-static void MX_ADC1_Init(void)
+static void MX_USART3_UART_Init(void)
 {
 
-  /* USER CODE BEGIN ADC1_Init 0 */
+  /* USER CODE BEGIN USART3_Init 0 */
 
-  /* USER CODE END ADC1_Init 0 */
+  /* USER CODE END USART3_Init 0 */
 
-  ADC_ChannelConfTypeDef sConfig = {0};
+  /* USER CODE BEGIN USART3_Init 1 */
 
-  /* USER CODE BEGIN ADC1_Init 1 */
-
-  /* USER CODE END ADC1_Init 1 */
-
-  /** Configure the global features of the ADC (Clock, Resolution, Data Alignment and number of conversion)
-  */
-  hadc1.Instance = ADC1;
-  hadc1.Init.ClockPrescaler = ADC_CLOCK_SYNC_PCLK_DIV8;
-  hadc1.Init.Resolution = ADC_RESOLUTION_12B;
-  hadc1.Init.ScanConvMode = DISABLE;
-  hadc1.Init.ContinuousConvMode = DISABLE;
-  hadc1.Init.DiscontinuousConvMode = DISABLE;
-  hadc1.Init.ExternalTrigConvEdge = ADC_EXTERNALTRIGCONVEDGE_NONE;
-  hadc1.Init.ExternalTrigConv = ADC_SOFTWARE_START;
-  hadc1.Init.DataAlign = ADC_DATAALIGN_RIGHT;
-  hadc1.Init.NbrOfConversion = 1;
-  hadc1.Init.DMAContinuousRequests = DISABLE;
-  hadc1.Init.EOCSelection = ADC_EOC_SINGLE_CONV;
-  if (HAL_ADC_Init(&hadc1) != HAL_OK)
+  /* USER CODE END USART3_Init 1 */
+  huart3.Instance = USART3;
+  huart3.Init.BaudRate = 19200;
+  huart3.Init.WordLength = UART_WORDLENGTH_8B;
+  huart3.Init.StopBits = UART_STOPBITS_1;
+  huart3.Init.Parity = UART_PARITY_NONE;
+  huart3.Init.Mode = UART_MODE_TX_RX;
+  huart3.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart3.Init.OverSampling = UART_OVERSAMPLING_16;
+  if (HAL_UART_Init(&huart3) != HAL_OK)
   {
     Error_Handler();
   }
+  /* USER CODE BEGIN USART3_Init 2 */
 
-  /** Configure for the selected ADC regular channel its corresponding rank in the sequencer and its sample time.
-  */
-  sConfig.Channel = ADC_CHANNEL_3;
-  sConfig.Rank = 1;
-  sConfig.SamplingTime = ADC_SAMPLETIME_480CYCLES;
-  if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  /* USER CODE BEGIN ADC1_Init 2 */
-
-  /* USER CODE END ADC1_Init 2 */
+  /* USER CODE END USART3_Init 2 */
 
 }
 
@@ -219,12 +254,23 @@ static void MX_ADC1_Init(void)
   */
 static void MX_GPIO_Init(void)
 {
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
   /* USER CODE BEGIN MX_GPIO_Init_1 */
 
   /* USER CODE END MX_GPIO_Init_1 */
 
   /* GPIO Ports Clock Enable */
-  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOD_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOD, GPIO_PIN_12|GPIO_PIN_13|GPIO_PIN_14|GPIO_PIN_15, GPIO_PIN_RESET);
+
+  /*Configure GPIO pins : PD12 PD13 PD14 PD15 */
+  GPIO_InitStruct.Pin = GPIO_PIN_12|GPIO_PIN_13|GPIO_PIN_14|GPIO_PIN_15;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOD, &GPIO_InitStruct);
 
   /* USER CODE BEGIN MX_GPIO_Init_2 */
 
